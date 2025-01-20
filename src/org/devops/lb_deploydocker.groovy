15a301167c5155416370fa0f2939a4f627f33fd9
DiegoGuimo <diegoguillen@periferia-it.com>
2025-01-20 12:08:25 -0500

    se corrige

--- src/org/devops/lb_deploydocker.groovy
+++ src/org/devops/lb_deploydocker.groovy
@@ -4,10 +4,15 @@ def despliegueContenedor (projectGitName) {
     // Detener y eliminar el contenedor previo si existe
     sh "docker stop ${projectGitName} || true"
     sh "docker rm ${projectGitName} || true"
+    
     // Descargar y ejecutar el contenedor
-    sh "docker pull diegoguimo182/${projectGitName}"
-    sh""" docker run -d --name ${projectGitName} \
-    --network=${env.NameNetwork} -p 5174:5174 \
-    --user root diegoguimo182/${projectGitName}:latest
+    echo "Pulling Docker image from Docker Hub..."
+    sh "docker pull diegoguimo182/${projectGitName}:latest"
+    
+    echo "Running Docker container..."
+    sh """
+        docker run -d --name ${projectGitName} \
+        --network=${env.NameNetwork} -p 5174:5174 \
+        --user root diegoguimo182/${projectGitName}:latest
     """
 }
