e48b5c808ad2cf5e8106ae477571e24237158cc9
DiegoGuimo <diegoguillen@periferia-it.com>
2025-01-20 13:19:37 -0500

    se corrige

--- src/org/devops/lb_owasp.groovy
+++ src/org/devops/lb_owasp.groovy
@@ -1,11 +1,12 @@
 package org.devops
 
 def AnalisisOwasp (projectGitName) {
-sh""" docker run --rm -v ProjectOwasp:/zap/wrk/:rw \
-    --user root --network=${env.NameNetwork} \
-    -t owasp/zap2docker-stable \
-    zap-full-scan.py \
-    -t ${env.dominio} \
-    -r ProjectOwasp.html -I
+    sh """
+    docker run --rm -v ProjectOwasp:/zap/wrk/:rw \
+        --user root --network=${env.NameNetwork} \
+        -t edansama96/zap2docker-stable \
+        zap-full-scan.py \
+        -t ${env.dominio} \
+        -r ProjectOwasp.html -I
     """
 }
