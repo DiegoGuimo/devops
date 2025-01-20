e26e47474b286647edb1be6e83202add106e4e1c
DiegoGuimo <diegoguillen@periferia-it.com>
2025-01-20 12:04:52 -0500

    se corrige

--- src/org/devops/lb_publicardockerhub.groovy
+++ src/org/devops/lb_publicardockerhub.groovy
@@ -1,9 +1,13 @@
 package org.devops
 
 def publicarImage(projectGitName) {
-    withCredentials([usernamePassword(credentialsId: "${env.TOKEN_ID}", passwordVariable: 'DOCKERHUB_PASSWORD', usernameVariable: 'DOCKERHUB_USERNAME')]) {
+    def sanitizedProjectName = projectGitName.replaceAll("[^a-zA-Z0-9.-]", "-")
+    
+    withCredentials([usernamePassword(credentialsId: "${env.TOKEN_ID}",
+    passwordVariable: 'DOCKERHUB_PASSWORD', usernameVariable: 'DOCKERHUB_USERNAME')]) {
         sh "docker login -u ${env.DOCKERHUB_USERNAME} -p ${env.DOCKERHUB_PASSWORD}"
-        sh "docker tag ${projectGitName}:latest ${env.DOCKERHUB_USERNAME}/${projectGitName}:latest"
-        sh "docker push ${env.DOCKERHUB_USERNAME}/${projectGitName}:latest"
+        // Etiquetar la imagen con el nombre completo y luego subirla a Docker Hub
+        sh "docker tag ${env.DOCKER_USERNAME}/${sanitizedProjectName}:latest ${env.DOCKERHUB_USERNAME}/${sanitizedProjectName}:latest"
+        sh "docker push ${env.DOCKERHUB_USERNAME}/${sanitizedProjectName}:latest"
     }
 }
