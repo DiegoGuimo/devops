ee8a8900cef1307152546bbd51c27b49c59dac91
DiegoGuimo <diegoguillen@periferia-it.com>
2025-01-20 11:53:50 -0500

    se corrige

--- src/org/devops/lb_buildimagen.groovy
+++ src/org/devops/lb_buildimagen.groovy
@@ -2,14 +2,17 @@ package org.devops
 
 def buildImageDocker(projectName) {
     def sanitizedProjectName = projectName.replaceAll("[^a-zA-Z0-9.-]", "-")
-    
+
+    // Asegurarse de que la imagen se construya correctamente con el nombre adecuado
     sh """
     echo "Docker username: ${env.DOCKER_USERNAME}"
-    echo "Project name: ${projectName}"
+    echo "Building image with the name: ${env.DOCKER_USERNAME}/${sanitizedProjectName}:latest"
     """
 
+    // Construir la imagen con el nombre correcto
     sh """
         docker build --no-cache -t ${env.DOCKER_USERNAME}/${sanitizedProjectName}:latest .
     """
+
     echo "Sanitized project name for Docker Image: ${sanitizedProjectName}"
 }
