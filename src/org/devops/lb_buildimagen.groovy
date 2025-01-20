0d2ff37dbebd7d912da2597335dfdef91a6bd757
DiegoGuimo <diegoguillen@periferia-it.com>
2025-01-20 11:49:37 -0500

    se corrige

--- src/org/devops/lb_buildimagen.groovy
+++ src/org/devops/lb_buildimagen.groovy
@@ -4,12 +4,12 @@ def buildImageDocker(projectName) {
     def sanitizedProjectName = projectName.replaceAll("[^a-zA-Z0-9.-]", "-")
     
     sh """
-    echo "Docker username: ${env.DOCKERHUB_USERNAME_USR}"
+    echo "Docker username: ${env.DOCKER_USERNAME}"
     echo "Project name: ${projectName}"
     """
 
     sh """
-        docker build --no-cache -t ${env.DOCKERHUB_USERNAME}/${sanitizedProjectName}:latest .
+        docker build --no-cache -t ${env.DOCKER_USERNAME}/${sanitizedProjectName}:latest .
     """
     echo "Sanitized project name for Docker Image: ${sanitizedProjectName}"
 }
--- src/org/devops/lb_publicardockerhub.groovy
+++ src/org/devops/lb_publicardockerhub.groovy
@@ -1,10 +1,9 @@
 package org.devops
 
 def publicarImage(projectGitName) {
-    withCredentials ([usernamePassword(credentialsId: "${env.TOKEN_ID}",
-    passwordVariable: 'DOCKERHUB_PASSWORD', usernameVariable: 'DOCKERHUB_USERNAME')]) {
+    withCredentials([usernamePassword(credentialsId: "${env.TOKEN_ID}", passwordVariable: 'DOCKERHUB_PASSWORD', usernameVariable: 'DOCKERHUB_USERNAME')]) {
         sh "docker login -u ${env.DOCKERHUB_USERNAME} -p ${env.DOCKERHUB_PASSWORD}"
-        sh "docker tag ${projectGitName} ${env.DOCKERHUB_USERNAME}/${projectGitName}:latest"
+        sh "docker tag ${projectGitName}:latest ${env.DOCKERHUB_USERNAME}/${projectGitName}:latest"
         sh "docker push ${env.DOCKERHUB_USERNAME}/${projectGitName}:latest"
     }
 }
